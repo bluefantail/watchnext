8226474448dc81113d330e63a3c16556409c61cc
Matt Fannin <matt.fannin@powershop.co.nz>
2016-10-24 21:26:54 +1300

    Testing to see if js is running in prod

--- app/assets/javascripts/watchlist.js
+++ app/assets/javascripts/watchlist.js
@@ -7,3 +7,5 @@ var omdbKey = 'c1fdf939';
 var omdbID = 'http://' + omdbUrl + '?tomatoes=true&i=';
 var omdbSearch = 'http://' + omdbUrl + '?type=movie&s=';
 var omdbPoster = 'http://img.' + omdbUrl + '?apikey=' + omdbKey + '&i=';
+
+console.log('hi')
